96fbd52a4f3ed30a966e22acb3882aef53ec57b2
Uday kiran <64728986+uday-kiran77@users.noreply.github.com>
2023-01-31 18:43:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,19 @@ app.use(express.json());
 
 app.use("/items", itemsRouter);
 
+// const test = async () => {
+//   const item = await Item.create({
+//     nftId: 1,
+//     contractAddress: "",
+//     originalMinter: "",
+//     size: "",
+//     drop: "",
+//   });
+//   const items = await Item.find();
+//   console.log(items);
+// };
+// test();
+
 app.listen(port, () => {
   console.log("listening on port " + port);
 });
